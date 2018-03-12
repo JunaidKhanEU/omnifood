8a476c98bc95077946ab6346a400b6e90ecbd0d9
junaid <junaidkhan1@gmx.com>
2018-03-12 16:37:04 +0000

    OmniFood Website

---
+++ 03-variable.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+<body>
+<div class="container">
+<h1>PHP up and running</h1>
+
+<div class="code-content">
+<?php
+ $name="Junaid";
+$age=35;
+ echo $name;
+ echo "<hr>";
+ echo $age;
+ $happy = true;
+ echo "<hr>";
+ echo $happy;
+
+
+ ?>
+</div>
+
+
+</div>
+
+
+
+
+</body>
+</html>
